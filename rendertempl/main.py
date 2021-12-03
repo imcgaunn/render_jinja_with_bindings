47c8b432602591d94ef430037549d4bd18bd6dc5
Ian McGaunn <ianmcgaunn@gmail.com>
2021-12-03 18:52:53 -0500

    run formatter and implement command line script

--- rendertempl/main.py
+++ rendertempl/main.py
@@ -1,6 +1,11 @@
 import argparse
 import logging
+import rich
 import sys
+
+import rendertempl.bindings as bindings
+import rendertempl.render as render
+
 from rich.logging import RichHandler
 from pathlib import Path
 
@@ -39,7 +44,29 @@ def parse_arguments(args):
     return opts
 
 
-def main(args):
-    options = parse_arguments(args)
+def main():
+    options = parse_arguments(sys.argv[1:])
     logger.info(f"parsed options: [{options}]")
+    rendered_template = ""
+    if options.directory:
+        logger.debug(
+            f"rendering template {options.template} with bindings files from {options.directory}"
+        )
+        rendered_template = render.render_template(
+            options.template, **bindings.load_from_directory(options.directory)
+        )
+    elif options.file:
+        logger.debug(
+            f"rendering template {options.template} with bindings from file {options.file}"
+        )
+        rendered_template = render.render_template(
+            options.template, **bindings.load_from_file(options.file)
+        )
+    else:
+        rich.print(
+            "i don't know what to do because neither a directory or file with bindings was specified"
+        )
+        sys.exit(42)
+
+    rich.print(rendered_template)
     sys.exit(0)
--- tests/test_bindings.py
+++ tests/test_bindings.py
@@ -7,11 +7,11 @@ logger = logging.getLogger(__name__)
 def test_can_load_bindings_from_file(bindings_dir):
     bindings_path = bindings_dir.joinpath("single_file.yml")
     variable_map = bindings.load_from_file(bindings_path)
-    assert variable_map['test_key'] == 'abcdefg'
+    assert variable_map["test_key"] == "abcdefg"
 
 
 def test_can_load_directory_of_files(bindings_dir):
     bindings_path = bindings_dir.joinpath("easy_dir_one")
     variable_map = bindings.load_from_directory(bindings_path)
     # files loaded later should have higher precedence
-    assert variable_map["override_me"] == 'third_val'
+    assert variable_map["override_me"] == "third_val"
