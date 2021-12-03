9c8d1728a948e29b6f60649a7374a6947ebec412
Ian McGaunn <ianmcgaunn@gmail.com>
2021-12-03 15:01:54 -0500

    add some helpful notes to self to bindings.py and scaffold out a test case for loading 'directory bindigns'

--- rendertempl/bindings.py
+++ rendertempl/bindings.py
@@ -20,4 +20,24 @@ def load_bindings_from_directory(dir_path: Path) -> Dict[Any, Any]:
     """ load bindings from a directory of .yml files in lexicographical precedence order """ 
     if not dir_path.is_dir():
         raise BadBindingsPathError(f"{dir_path} is not a directory and can't contain yaml files with bindings")
+    # TODO: actually load the bindings
+    # NB: this seems like a pretty good use for `collections.ChainMap`
+    # Help on class ChainMap in collections:
+    # collections.ChainMap = class ChainMap(collections.abc.MutableMapping)
+    #  |  collections.ChainMap(*maps)
+    #  |
+    #  |  A ChainMap groups multiple dicts (or other mappings) together
+    #  |  to create a single, updateable view.
+    #  |
+    #  |  The underlying mappings are stored in a list.  That list is public and can
+    #  |  be accessed or updated using the *maps* attribute.  There is no other
+    #  |  state.
+    #  |
+    #  |  Lookups search the underlying mappings successively until a key is found.
+    #  |  In contrast, writes, updates, and deletions only operate on the first
+    #  |  mapping.
+
+    # all one would have to do is call `load_bindings_from_file` on each file
+    # in the directory in the proper order and add each of the resulting dictionaries
+    # to a chainmap in the same order.
     return {}
