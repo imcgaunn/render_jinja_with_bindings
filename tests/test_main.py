4456a6118985ab2e2bf53b419aad21f2921975af
Ian McGaunn <ianmcgaunn@gmail.com>
2021-12-03 19:15:54 -0500

    add load/render test representative of what a user running the command line script would want

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 import logging
 import rendertempl.main as main
+import rendertempl.render as render
+import rendertempl.bindings as bindings
 import pathlib
 import pytest
 
@@ -26,3 +28,10 @@ def test_only_one_bindings_source_allowed():
     assert opts.file == pathlib.Path("wow.txt")
     opts = main.parse_arguments(["-d", "/tmp/wow", "template.j2"])
     assert opts.directory == pathlib.Path("/tmp/wow")
+
+
+def test_load_and_render_complex_directory_bindings(bindings_dir, jinja_templates_dir):
+    complex_bindings = bindings.load_from_directory(bindings_dir.joinpath("complex_dir_one"))
+    boring_template_path = jinja_templates_dir.joinpath("interactive.j2")
+    rendered = render.render_template(boring_template_path, **complex_bindings)
+    logger.debug(rendered)
